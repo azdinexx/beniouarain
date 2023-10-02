ab1896606746fafcbd2094bcc4eb95eb32e86002
azdinexx <azdinebouali2002@gmail.com>
2023-10-02 10:22:48 +0100

    fixed locomotive not working after route changes

--- app/locomotive.tsx
+++ app/locomotive.tsx
@@ -1,13 +1,15 @@
 'use client';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { useEffect } from 'react';
 
 export function Locomotive({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
   useEffect(() => {
     (async () => {
       const LocomotiveScroll = (await import('locomotive-scroll')).default;
       const locomotiveScroll = new LocomotiveScroll();
     })();
-  }, []);
+  }, [pathname]);
   return <div>{children}</div>;
 }
