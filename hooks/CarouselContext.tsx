1762ebca354b1b3cccc5b90ce49060dc84b86b3d
azdinexx <azdinebouali2002@gmail.com>
2023-10-10 14:23:48 +0100

    added types for context

--- hooks/CarouselContext.tsx
+++ hooks/CarouselContext.tsx
@@ -2,7 +2,19 @@
 import ImageCaroussel from '@/components/ProductsDetailsPage/ImageCaroussel';
 import { Dispatch, SetStateAction, createContext, useState } from 'react';
 
-export const CarouselContext = createContext({});
+interface Props {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  setimages: Dispatch<SetStateAction<never[]>>;
+  setCurrent: Dispatch<SetStateAction<number>>;
+}
+
+export const CarouselContext = createContext<Props>({
+  isOpen: false,
+  setIsOpen: () => {},
+  setimages: () => {},
+  setCurrent: () => {},
+});
 
 export function CarouselProvider({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false);
