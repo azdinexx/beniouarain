83bc7991f7fcdffbe8b42ef4d6cfc5fe4b897cd5
azdinexx <azdinebouali2002@gmail.com>
2023-10-11 17:47:04 +0100

    ddd

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react';
 
+export const metadata: Metadata = {
+  title: 'All Products',
+  description: 'All Products Page for the Beni ourain rugs shopify store',
+};
 function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
