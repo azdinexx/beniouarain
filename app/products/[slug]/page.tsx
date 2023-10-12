b0c6986604693e5a60be5933dc5859219ce059db
azdinexx <azdinebouali2002@gmail.com>
2023-10-12 12:22:28 +0100

    try catch block

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -7,22 +7,27 @@ import { Product } from '@/lib/shopify/types';
 
 async function page({ params }: { params: { slug: string } }) {
   const { slug } = params;
-  const product = await getProduct(slug);
-  const recommended = await getProducts({
-    query: `tag:rugs}`,
-  });
-  console.log(recommended);
-  const images = product?.images.map((img) => img.url);
+  try {
+    const product = await getProduct(slug);
 
-  return (
-    <>
-      <div className='max-w-7xl  mx-auto  grid grid-cols-5 gap-3 mt-6'>
-        <Images images={images as string[]} />
-        <Details product={product as Product} />
-        <RecommendedProducts data={recommended} />
-      </div>
-    </>
-  );
+    const recommended = await getProducts({
+      query: `tag:rugs}`,
+    });
+    console.log(recommended);
+    const images = product?.images.map((img) => img.url);
+
+    return (
+      <>
+        <div className='max-w-7xl  mx-auto  grid grid-cols-5 gap-3 mt-6'>
+          <Images images={images as string[]} />
+          <Details product={product as Product} />
+          <RecommendedProducts data={recommended} />
+        </div>
+      </>
+    );
+  } catch {
+    return <div>Product not found</div>;
+  }
 }
 
 export default page;
