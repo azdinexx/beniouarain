18a2187bac4e0e5fb685951a1eeb38ed09c866aa
azdinexx <azdinebouali2002@gmail.com>
2023-10-12 11:50:38 +0100

    llllllll

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -57,7 +57,7 @@ import {
 const domain = process.env.SHOPIFY_STORE_DOMAIN
   ? ensureStartsWith(process.env.SHOPIFY_STORE_DOMAIN, 'https://')
   : '';
-const endpoint = `${domain}/api/2023-01/graphql.json`;
+const endpoint = `https://artossi.myshopify.com/api/2023-01/graphql.json`;
 const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN!;
 
 type ExtractVariables<T> = T extends { variables: object }
