96d847b93a27b786dff4b80838ac506b370f5346
azdinexx <azdinebouali2002@gmail.com>
2023-10-16 18:03:53 +0100

    try this trick

--- components/Desktop/index.tsx
+++ components/Desktop/index.tsx
@@ -8,7 +8,9 @@ import { Collection, Product } from '@/lib/shopify/types';
 import { useEffect, useState } from 'react';
 
 // Client Components:
-const ComponentB = dynamic(() => import('./Desktop'));
+const Desktop = dynamic(() => import('./Desktop'));
+const Carouselo = dynamic(() => import('../Mobile/carousel'));
+const Collectionso = dynamic(() => import('../Mobile/Collections'));
 
 export default function MobileOrDesktop({
   Carousel_products,
@@ -21,16 +23,17 @@ export default function MobileOrDesktop({
   useEffect(() => {
     setisclient(true);
   }, []);
+  console.log(isMobile);
 
   if (!isclient) return null;
   return (
     <div className='hidden md:block'>
       {/* Load on demand, only when/if the condition is met */}
-      {!isMobile && <ComponentB />}
+      {!isMobile && <Desktop />}
       {isMobile && (
         <>
-          <Carousel products={Carousel_products} />
-          <Collections collections={collections} />
+          <Carouselo products={Carousel_products} />
+          <Collectionso collections={collections} />
         </>
       )}
     </div>
