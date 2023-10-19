b73b24627b3d2ad74d9cf79e44463036881b0722
azdinexx <azdinebouali2002@gmail.com>
2023-10-19 17:06:36 +0100

    no lenis for mobile

--- components/shared/locomotive.tsx
+++ components/shared/locomotive.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
+import { isMobile } from 'react-device-detect';
 
 export function Locomotive({ children }: { children: React.ReactNode }) {
   const [dimension, setDimension] = React.useState({
@@ -10,24 +11,27 @@ export function Locomotive({ children }: { children: React.ReactNode }) {
   });
 
   useEffect(() => {
-    const lenis = new Lenis();
+    if (isMobile) {
+      return;
+    } else {
+      const lenis = new Lenis();
 
-    const raf = (time: any) => {
-      lenis.raf(time);
+      const raf = (time: any) => {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      };
+
+      const resize = () => {
+        setDimension({ width: window.innerWidth, height: window.innerHeight });
+      };
+
+      window.addEventListener('resize', resize);
       requestAnimationFrame(raf);
-    };
-
-    const resize = () => {
-      setDimension({ width: window.innerWidth, height: window.innerHeight });
-    };
-
-    window.addEventListener('resize', resize);
-    requestAnimationFrame(raf);
-    resize();
-
-    return () => {
-      window.removeEventListener('resize', resize);
-    };
+      resize();
+      return () => {
+        window.removeEventListener('resize', resize);
+      };
+    }
   }, []);
   return <div>{children}</div>;
 }
