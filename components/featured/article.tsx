8f4b0c5e27311f4123a40916d0a288dae27bdfa4
azdinexx <azdinebouali2002@gmail.com>
2023-10-19 16:00:33 +0100

    added something

---
+++ components/featured/article.tsx
@@ -0,0 +1,80 @@
+'use client';
+import { Product } from '@/lib/shopify/types';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useTransform, useScroll, motion } from 'framer-motion';
+import Lenis from '@studio-freight/lenis';
+
+interface Props {
+  reverse?: boolean;
+  product: Product;
+  handle: string;
+  children: React.ReactNode;
+}
+export function Article({ product, reverse = false, handle, children }: Props) {
+  const image = useRef(null);
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+
+  const { scrollYProgress } = useScroll({
+    target: image,
+    offset: ['start end', 'end start'],
+  });
+  const { height } = dimension;
+  const y = useTransform(scrollYProgress, [0, 1], [0, (height - 100) * 0.3]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, (height - 200) * 0.6]);
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    const raf = (time: any) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    const resize = () => {
+      setDimension({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener('resize', resize);
+    requestAnimationFrame(raf);
+    resize();
+
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
+  return (
+    <article className='grid grid-cols-1 md:grid-cols-2   overflow-hidden'>
+      <div className={`${reverse && 'md:order-2'} flex flex-col gap-3 p-10`}>
+        <Link href={'/all/' + handle}>
+          <h2 className='text-xl font-bold max-w-xs hover:underline'>
+            {product.title}
+          </h2>
+        </Link>
+        <p className='py-3'>{product.description.substring(0, 320) + '...'}</p>
+        <p className='font-bold pb-3'>
+          ${product.priceRange.maxVariantPrice.amount}{' '}
+        </p>
+        {children}
+      </div>
+      <motion.div
+        className='relative'
+        ref={image}
+        style={reverse ? { y: y } : { y: y2 }}
+      >
+        <Image
+          width={2000}
+          height={2000}
+          src={product.featuredImage.url}
+          alt={product.featuredImage.altText}
+          className={reverse ? '-translate-y-0' : '-translate-y-20'}
+          priority={false}
+          loading='lazy'
+          blurDataURL={imagePlaceHolder}
+        />
+      </motion.div>
+    </article>
+  );
+}
+
+const imagePlaceHolder = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 2000'%3E%3C/svg%3E`;
--- components/featured/index.tsx
+++ components/featured/index.tsx
@@ -5,6 +5,7 @@ import { Product } from '@/lib/shopify/types';
 import { AddToCart } from '../cart/add-to-cart';
 import './featured.css';
 import Link from 'next/link';
+import { Article } from './article';
 
 async function Featured() {
   const products = await getProducts({
@@ -20,7 +21,7 @@ async function Featured() {
       >
         Featured Products This Week
       </h2>
-      <section className='grid grid-cols-1   gap-10     '>
+      <section className='grid grid-cols-1   gap-20     '>
         {products.map((product, i) =>
           product === null ? null : (
             <Article
@@ -28,7 +29,12 @@ async function Featured() {
               product={product}
               reverse={i % 2 === 1}
               handle={product.handle}
-            />
+            >
+              <AddToCart
+                variants={product.variants}
+                availableForSale={product.availableForSale}
+              />
+            </Article>
           )
         )}
       </section>
@@ -37,47 +43,3 @@ async function Featured() {
 }
 
 export default Featured;
-
-function Article({
-  product,
-  reverse = false,
-  handle,
-}: {
-  reverse?: boolean;
-  product: Product;
-  handle: string;
-}) {
-  return (
-    <article className='grid grid-cols-1 md:grid-cols-2  '>
-      <div className={`${reverse && 'md:order-2'} flex flex-col gap-3 p-10`}>
-        <Link href={'/all/' + handle}>
-          <h2 className='text-xl font-bold max-w-xs hover:underline'>
-            {product.title}
-          </h2>
-        </Link>
-        <p className='py-3'>{product.description.substring(0, 320) + '...'}</p>
-        <p className='font-bold pb-3'>
-          ${product.priceRange.maxVariantPrice.amount}{' '}
-        </p>
-        <AddToCart
-          variants={product.variants}
-          availableForSale={product.availableForSale}
-        />
-      </div>
-      <div data-scroll data-scroll-speed='0.4'>
-        <Image
-          width={2000}
-          height={2000}
-          src={product.featuredImage.url}
-          alt={product.featuredImage.altText}
-          className={reverse ? '-translate-y-0' : '-translate-y-20'}
-          priority={false}
-          loading='lazy'
-          blurDataURL={imagePlaceHolder}
-        />
-      </div>
-    </article>
-  );
-}
-
-const imagePlaceHolder = `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 2000'%3E%3C/svg%3E`;
