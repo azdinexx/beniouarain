da26dcd6e0bc83422efc600a8a6eac53b72bf7d1
azdinexx <azdinebouali2002@gmail.com>
2023-10-27 12:24:12 +0100

    optimized the header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Cart from '../cart';
 import { getCollections } from '@/lib/shopify';
-import Nav, { ClientNav } from './Nav';
+import { ClientNav } from './Nav';
 
 import Link from 'next/link';
+import dynamic from 'next/dynamic';
 function Header() {
   async function getoCollections() {
     'use server';
     return getCollections();
   }
+  const Nav = dynamic(() => import('./Nav'), {
+    ssr: false,
+  });
   return (
     <>
       <header className=' container px-3 md:px-0 sticky top-0 z-[50]  mx-auto flex justify-between items-center bg-white py-3 '>
