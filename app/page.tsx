3874bd8e0524b1fffc6046f909dd631dc6223559
azdinexx <azdinebouali2002@gmail.com>
2023-10-31 17:15:49 +0100

    added metadata to /home

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,28 @@ import React from 'react';
 import Caroussel from '@/components/landing/Caroussel';
 import Hero from '@/components/hero';
 import BlogPosts from '@/components/BlogPosts';
+import { Metadata } from 'next';
+import homeOpenGraph from '@/public/open-graph/home.jpg';
 
+// MetaData
+export const metadata: Metadata = {
+  title: 'Beni Ouarain Carpet | Authentic Moroccan Rugs',
+  description:
+    'discover our collection of authentic moroccan rugs and shop the best Beni Ouarain rugs online.',
+  openGraph: {
+    title: 'Beni Ouarain Carpet | Authentic Moroccan Rugs',
+    description:
+      'discover our collection of authentic moroccan rugs and shop the best Beni Ouarain rugs online.',
+    images: [
+      {
+        url: homeOpenGraph.src,
+        width: 1200,
+        height: 627,
+        alt: 'Beni Ouarain Carpet | Authentic Moroccan Rugs',
+      },
+    ],
+  },
+};
 async function Page() {
   const Carousel_products = await getProducts({
     query: 'tag:authentic_rug',
